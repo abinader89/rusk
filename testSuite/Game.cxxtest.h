c1b2b91ed50a8e403cfef79b721ea06e10de2003
Abinader <abinader@ccs.neu.edu>
2018-12-03 15:27:43 -0500

    abinader89: allocated on heap and not deleted in function for now.

--- testSuite/Game.cxxtest.h
+++ testSuite/Game.cxxtest.h
@@ -8,9 +8,9 @@ class GameTestSuite : public CxxTest::TestSuite
 {
     public:
         void testLoadSurface() {
-            Game test_game = Game();
+            Game* test_game = new Game();
             std::string test_string = "bmps/hexSelected.bmp";
-            SDL_Surface* test_surface = test_game.loadSurface(test_string);
+            SDL_Surface* test_surface = test_game->loadSurface(test_string);
 
             TS_ASSERT_EQUALS(test_surface->w, 100);
             TS_ASSERT_EQUALS(test_surface->h, 100);
